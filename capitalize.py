86a3847eeada08c6721a7796f143daeb1656ea4b
scand1ce <61993238+scand1ce@users.noreply.github.com>
2020-03-31 20:31:51 +0300

    Add files via upload

---
+++ capitalize.py
@@ -0,0 +1,55 @@
+﻿'''
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Indexed capitalization
+
+Even-odd disparity
+'''
+
+##########################################################################################################################################
+s = "abcdadfgdfgadfgef"
+
+def capitalize(s):
+    arr = []
+    arr2 = []
+    c = 0
+    c1 = 0
+    while c < len(s):
+        for j in s:
+            if c%2 == 0 or c == 0:
+                arr.append(j.upper())
+            elif c%2 != 0:
+                arr.append(j.lower())
+
+
+            c+=1
+
+    while c1 < len(s):
+        for x in s:
+            if c1 % 2 == 0 or c1 == 0:
+                arr2.append(x.lower())
+            elif c1%2 != 0:
+                arr2.append(x.upper())
+
+
+
+            c1+=1
+
+    return [''.join(arr),''.join(arr2)]
+
+print(capitalize(s))
+###############################################################################
+def capitalize(s):
+    result = ['','']
+    for i,c in enumerate(s):
+        result[0] += c.lower() if i % 2 else c.upper()
+        result[1] += c.upper() if i % 2 else c.lower()
+    return result
---
+++ remove_every_other.py
@@ -0,0 +1,21 @@
+﻿'''
+Take an array and remove every second element out of that array. Always keep the first element and start removing with the next element.
+
+Example:
+
+my_list = ['Keep', 'Remove', 'Keep', 'Remove', 'Keep', ...]
+None of the arrays will be empty, so you don't have to worry about that!
+
+'''
+
+##########################################################################################################################################
+def remove_every_other(my_list):
+        del my_list[1::2]
+        return my_list
+
+print(remove_every_other(my_list))
+#############################################################################
+ def remove_every_other(my_list):
+return my_list[::2]
+##########################################################################################################
+
